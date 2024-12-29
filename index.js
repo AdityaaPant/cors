566005a1e87df9fe599d71c44e4dd883dddf342a
Aditya Pant <pantanupam05@gmail.com>
2024-12-29 22:34:42 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.post("/sum", function (req, res) {
-	const a = parseInt(req.body.a);
-	const b = parseInt(req.body.b);
-	res.json({
-		answer: a + b,
-	});
-});
-app.listen(3000);
+// app.use(express.json());
+// app.use(cors());
+// app.post("/sum", function (req, res) {
+// 	const a = parseInt(req.body.a);
+// 	const b = parseInt(req.body.b);
+// 	res.json({
+// 		answer: a + b,
+// 	});
+// });
+// app.listen(3000);
+
+// const input = [1, 2, 3, 4, 5];
+// function transform(i) {
+// 	return i * 2;
+// }
+// const ans = input.map(transform);
+// console.log(ans);
+
+// const array = ["aditya", "ayush", "kirti"];
+
+// const ans = array.filter((n) => {
+// 	if (n.startsWith("k")) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// });
+// console.log(ans);
